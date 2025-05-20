198ad2a5152ecdb92aab755cbb5574255f072271
hafaris6 <aldyryalkhal150@gmail.com>
2025-05-20 16:31:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "أهلاً بك في الفرات"
+
+if __name__ == "__main__":
+    app.run()
 import os
 import uuid
 import logging
