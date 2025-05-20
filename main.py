177101d5da2699e1267d07217b9bc09953797e4a
hafaris6 <aldyryalkhal150@gmail.com>
2025-05-20 16:32:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from app import app  # noqa: F401
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "أهلاً بك في الفرات"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run()
